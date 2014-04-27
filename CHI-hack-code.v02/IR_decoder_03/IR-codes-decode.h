448c14c910939ca4ff02834f1e68bd646039199a
Antonio Gamba-Bari <lodeanto@gmail.com>
2014-04-27 11:01:41 -0400

    IR-codes-decode.h

---
+++ CHI-hack-code.v02/IR_decoder_03/IR-codes-decode.h
@@ -0,0 +1,55 @@
+
+int IRsignalDecode1[] = {	
+// ON, OFF (in 10's of microseconds)
+	66, 54,
+	86, 44,
+	98, 48,
+	110, 48,
+	124, 50,
+	138, 48,
+	154, 0
+};
+	
+int IRsignalDecode2[] = {
+// ON, OFF (in 10's of microseconds)
+	210, 100,
+	226, 96,
+	240, 100,
+	254, 100,
+	268, 100,
+	280, 98,
+	296, 0
+};
+
+int IRsignalDecode3[] = {	
+// ON, OFF (in 10's of microseconds)
+	492, 146,
+	506, 146,
+	520, 146,
+	536, 144,
+	546, 146,
+	562, 146,
+	570, 0
+};
+
+int IRsignalDecode4[] = {
+// ON, OFF (in 10's of microseconds)
+	622, 188,
+	626, 194,
+	642, 194,
+	656, 194,
+	668, 194,
+	684, 194,
+	700, 0
+};
+
+int IRsignalDecode5[] = {
+// ON, OFF (in 10's of microseconds)
+	752, 242,
+	770, 238,
+	786, 236,
+	796, 240,
+	806, 242,
+	816, 246,
+	834, 0
+};
